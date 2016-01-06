27fece8b64d26578fe7a3ee59f041f6557e355bb
VaneRaklan <antoniowii@yahoo.com>
2016-01-05 20:37:50 -0800

    (OI class, Joystick code) I added in the joystick code in the OI so the driver and manipulator joysticks are declared and ported as well as the buttons on each of said controller (declared and ported). I also made the outline for the RobotMap, meaning I made declarations for all of the parts, but did not port anything yet.

--- SOTABots_2014_revamp/src/org/usfirst/frc/team2557/robot/OI.java
+++ SOTABots_2014_revamp/src/org/usfirst/frc/team2557/robot/OI.java
@@ -1,6 +1,8 @@
 package org.usfirst.frc.team2557.robot;
 
-import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.buttons.*;
+
 import org.usfirst.frc.team2557.robot.commands.ExampleCommand;
 
 /**
@@ -34,5 +36,87 @@ public class OI {
     // Start the command when the button is released  and let it run the command
     // until it is finished as determined by it's isFinished method.
     // button.whenReleased(new ExampleCommand());
+	public Joystick joystick1;
+	public Joystick joystick2;
+	
+	//Driver Joystick Buttons
+	
+	public	JoystickButton LJ1;
+	public	JoystickButton LJ2;
+	public	JoystickButton LJ3;
+	public	JoystickButton LJ4;
+	public	JoystickButton LJ5;
+	public	JoystickButton LJ6;
+	public	JoystickButton LJ7;
+	public	JoystickButton LJ8;
+	public	JoystickButton LJ9;
+	public	JoystickButton LJ10;
+	public	JoystickButton LJ11;
+	public	JoystickButton LJ12;
+			
+			//Manipulator Joystick Buttons
+			
+	public JoystickButton RJ1;
+	public JoystickButton RJ2;
+	public JoystickButton RJ3;
+	public JoystickButton RJ4;
+	public JoystickButton RJ5;
+	public JoystickButton RJ6;
+	public JoystickButton RJ7;
+	public JoystickButton RJ8;
+	public JoystickButton RJ9;
+	public JoystickButton RJ10;
+	public JoystickButton RJ11;
+	public JoystickButton RJ12;
+
+	
+	
+	public OI(){
+		
+		joystick1 = new Joystick(0);
+		joystick2 = new Joystick(1);
+		
+		LJ1 = new JoystickButton(joystick1, 0);
+		LJ2 = new JoystickButton(joystick1, 1);
+		LJ3 = new JoystickButton(joystick1, 2);
+		LJ4 = new JoystickButton(joystick1, 3);
+		LJ5 = new JoystickButton(joystick1, 4);
+		LJ6 = new JoystickButton(joystick1, 5);
+		LJ7 = new JoystickButton(joystick1, 6);
+		LJ8 = new JoystickButton(joystick1, 7);
+		LJ9 = new JoystickButton(joystick1, 8);
+		LJ10 = new JoystickButton(joystick1,9);
+		LJ11 = new JoystickButton(joystick1, 10);
+		LJ12 = new JoystickButton(joystick1, 11);
+		
+		RJ1 = new JoystickButton(joystick2, 0);
+		RJ2 = new JoystickButton(joystick2, 1);
+		RJ3 = new JoystickButton(joystick2, 2);
+		RJ4 = new JoystickButton(joystick2, 3);
+		RJ5 = new JoystickButton(joystick2, 4);
+		RJ6 = new JoystickButton(joystick2, 5);
+		RJ7 = new JoystickButton(joystick2, 6);
+		RJ8 = new JoystickButton(joystick2, 7);
+		RJ9 = new JoystickButton(joystick2, 8);
+		RJ10 = new JoystickButton(joystick2,9);
+		RJ11 = new JoystickButton(joystick2, 10);
+		RJ12 = new JoystickButton(joystick2, 11);
+		
+		
+		
+		
+	}
+	
+	public Joystick joystick1(){
+		return joystick1;
+	}
+	public Joystick joystick2(){
+		return joystick2;
+	}
+	
+	
+	
+	
+	
 }
 
--- SOTABots_2014_revamp/src/org/usfirst/frc/team2557/robot/RobotMap.java
+++ SOTABots_2014_revamp/src/org/usfirst/frc/team2557/robot/RobotMap.java
@@ -1,4 +1,7 @@
 package org.usfirst.frc.team2557.robot;
+
+import edu.wpi.first.wpilibj.*;
+
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
@@ -15,4 +18,31 @@ public class RobotMap {
     // number and the module. For example you with a rangefinder:
     // public static int rangefinderPort = 1;
     // public static int rangefinderModule = 1;
+	
+	public static SpeedController frontLeft;
+	public static SpeedController frontRight;
+	public static SpeedController backLeft;
+	public static SpeedController backRight;
+	public static SpeedController winchMotor;
+	
+	public static DoubleSolenoid intakeArm;
+	public static DoubleSolenoid winchSolenoid;
+	public static DoubleSolenoid driveShift;
+	
+	public static DigitalInput hallEffect;
+	public static DigitalInput lightSensor;
+	public static Counter leftDriveEncoder;
+	public static Counter rightDriveEncoder;
+	public static Counter winchEncoder;
+	
+	
+	public static void init(){
+		
+		
+		
+	}
+	
+	
+	
+	
 }
