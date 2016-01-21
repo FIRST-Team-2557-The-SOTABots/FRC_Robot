10d8257dc00bb3854133c37f846528744353f4e7
Daemien <daemienz@gmail.com>
2016-01-21 14:40:10 -0800

    Added moat Autonomous
    Added another command called Moat_Auto.java.

---
+++ SOTABots2016/src/org/usfirst/frc/team2557/robot/commands/Moat_Auto.java
@@ -0,0 +1,46 @@
+package org.usfirst.frc.team2557.robot.commands;
+
+import org.usfirst.frc.team2557.robot.Robot;
+import org.usfirst.frc.team2557.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class Moat_Auto extends Command {
+
+    public Moat_Auto() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.autonomous);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	
+    	//This will only work if the rangeFinders are in the Front. Added 1/21/16.
+    	if(RobotMap.RangeFinderRight1.getVoltage()>10 && RobotMap.RangeFinderLeft2.getVoltage()>10){
+    		RobotMap.arcadeDrive.arcadeDrive(1, 0);
+    	}
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return RobotMap.RangeFinderRight1.getVoltage()<=10 && RobotMap.RangeFinderLeft2.getVoltage()<=10;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	RobotMap.arcadeDrive.arcadeDrive(0, 0);
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
