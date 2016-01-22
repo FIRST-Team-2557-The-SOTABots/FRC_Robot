0a32e2e0423027a8846d3620fedd71555aca3f2a
Daemien <daemienz@gmail.com>
2016-01-22 11:19:00 -0800

    Added Rockwall command
    Added the Rockwall autonomous.

---
+++ SOTABots2016/src/org/usfirst/frc/team2557/robot/commands/RockWall_Auto.java
@@ -0,0 +1,48 @@
+package org.usfirst.frc.team2557.robot.commands;
+
+import org.usfirst.frc.team2557.robot.Robot;
+import org.usfirst.frc.team2557.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class RockWall_Auto extends Command {
+
+    public RockWall_Auto() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.autonomous);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	
+    	//Will only work if the RangeFineders are in the Front. and above the drive train. Added 1/22/16.
+    	if(RobotMap.RangeFinderRight1.getVoltage()>10 && RobotMap.RangeFinderLeft2.getVoltage()>10){
+    		
+    		RobotMap.arcadeDrive.arcadeDrive(.5,0);
+    	}
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return RobotMap.RangeFinderRight1.getVoltage()<=10 && RobotMap.RangeFinderLeft2.getVoltage()<=10;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	
+    	RobotMap.arcadeDrive.arcadeDrive(0, 0);
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
