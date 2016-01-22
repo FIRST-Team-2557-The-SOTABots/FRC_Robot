222df1e55200b5de169436730398af6cf25434fe
IamParadoxic <Harrisonmcd1999@hotmail.com>
2016-01-22 13:24:54 -0800

    Added some new buttons to the OI, there not tied to any commands yet

--- SOTABots2016/src/org/usfirst/frc/team2557/robot/OI.java
+++ SOTABots2016/src/org/usfirst/frc/team2557/robot/OI.java
@@ -18,11 +18,30 @@ public class OI {
 	
 	public JoystickButton s1I;
 	public JoystickButton s1II;
+	public JoystickButton s1III;
+	public JoystickButton s1IV;
+	public JoystickButton s1V;
+	public JoystickButton s1VI;
+	public JoystickButton s1VII;
+	public JoystickButton s1VIII;
+	public JoystickButton s1IX;
+	
+	
+	
 	
     public OI(){
     	
-    	s1I = new JoystickButton(stick1, 0);
-    	s1II = new JoystickButton(stick1, 1);
+    	s1I = new JoystickButton(stick1, 1);
+    	s1II = new JoystickButton(stick1, 2);
+    	s1III = new JoystickButton(stick1, 3);
+    	s1IV = new JoystickButton(stick1, 4);
+    	s1V = new JoystickButton(stick1, 5);
+    	s1VI = new JoystickButton(stick1, 6);
+    	s1VII = new JoystickButton(stick1, 7);
+    	s1VIII = new JoystickButton(stick1, 8);
+    	s1IX = new JoystickButton(stick1, 9);
+    	
+    	
     	
     	
     	
