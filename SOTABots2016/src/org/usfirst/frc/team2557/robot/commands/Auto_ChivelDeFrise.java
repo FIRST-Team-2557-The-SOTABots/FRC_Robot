664f15e9d47244e425a20309731d3d6a2ceb6a63
Daemien <daemienz@gmail.com>
2016-01-23 12:50:23 -0800

    Finished all the beginning movement
    All autonomouses have some type of movement command now.

--- SOTABots2016/src/org/usfirst/frc/team2557/robot/commands/Auto_ChivelDeFrise.java
+++ SOTABots2016/src/org/usfirst/frc/team2557/robot/commands/Auto_ChivelDeFrise.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team2557.robot.commands;
 
+import org.usfirst.frc.team2557.robot.RobotMap;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -18,15 +20,21 @@ public class Auto_ChivelDeFrise extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	
+    	//Modified on 1/22/16.
+    	if(RobotMap.RangeFinderF.getVoltage()>4 ){
+    		RobotMap.arcadeDrive.arcadeDrive(1, 0);
+    	}
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return RobotMap.RangeFinderF.getVoltage()<=4;
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	RobotMap.arcadeDrive.arcadeDrive(0, 0);
     }
 
     // Called when another command which requires one or more of the same
