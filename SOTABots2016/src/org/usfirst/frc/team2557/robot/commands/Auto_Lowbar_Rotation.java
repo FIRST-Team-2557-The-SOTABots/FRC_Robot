80bff4b5db5265792cf2eca360559b1e351d42a4
Daemien <daemienz@gmail.com>
2016-01-23 13:02:09 -0800

    Finished LowBar Autonomous code

--- SOTABots2016/src/org/usfirst/frc/team2557/robot/commands/Auto_Lowbar_Rotation.java
+++ SOTABots2016/src/org/usfirst/frc/team2557/robot/commands/Auto_Lowbar_Rotation.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team2557.robot.commands;
 
+import org.usfirst.frc.team2557.robot.RobotMap;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -18,16 +20,23 @@ public class Auto_Lowbar_Rotation extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	
+    	if(RobotMap.RangeFinderF.getVoltage()>3){
+    		RobotMap.arcadeDrive.arcadeDrive(1, 0);
+    	}
+    	else if(RobotMap.RangeFinderF.getVoltage()<=3){
+    		RobotMap.arcadeDrive.arcadeDrive(0, 1);
+    	}
+    		
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return RobotMap.RangeFinderL.getVoltage()<=3;
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	RobotMap.arcadeDrive.arcadeDrive(0, 0);
     }
 
     // Called when another command which requires one or more of the same
