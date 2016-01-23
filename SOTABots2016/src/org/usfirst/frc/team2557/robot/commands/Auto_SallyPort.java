aa0746a65db3edadd9339eb0e832c35bf799f913
Daemien <daemienz@gmail.com>
2016-01-23 10:37:29 -0800

    Added Code to the sally port
    Auto_Sallyport.java has code that will allow the robot to reach the
    defense.

--- SOTABots2016/src/org/usfirst/frc/team2557/robot/commands/Auto_SallyPort.java
+++ SOTABots2016/src/org/usfirst/frc/team2557/robot/commands/Auto_SallyPort.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team2557.robot.commands;
 
+import org.usfirst.frc.team2557.robot.RobotMap;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -18,15 +20,23 @@ public class Auto_SallyPort extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	
+    	//This code is pretty much the same as the Draw Bridge but is for the sally port instead
+    	//and like always this will only work if the rangeFinders are in the front to register the doors of the drawbridge and the sally port
+    	//Added 1/22/16.
+    	if(RobotMap.RangeFinderRight1.getVoltage()>4 && RobotMap.RangeFinderLeft2.getVoltage()>4){
+    		RobotMap.arcadeDrive.arcadeDrive(1, 0);
+    	}
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return RobotMap.RangeFinderRight1.getVoltage()<=4 && RobotMap.RangeFinderLeft2.getVoltage()<=4;
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	RobotMap.arcadeDrive.arcadeDrive(0, 0);
     }
 
     // Called when another command which requires one or more of the same
