861a113925d2d8cf68df01e196a21af9585d1860
Daemien <daemienz@gmail.com>
2016-01-25 13:11:55 -0800

    Added sally port and rough terrain autos

---
+++ SOTABots2016/src/org/usfirst/frc/team2557/robot/commands/Auto_RoughtTerrain_CommandGroup.java
@@ -0,0 +1,30 @@
+package org.usfirst.frc.team2557.robot.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class Auto_RoughtTerrain_CommandGroup extends CommandGroup {
+    
+    public  Auto_RoughtTerrain_CommandGroup() {
+    	addSequential(new Auto_RoughTerrain());
+    	addSequential(new Auto_RoughTerrain_Rotation());
+        // Add Commands here:
+        // e.g. addSequential(new Command1());
+        //      addSequential(new Command2());
+        // these will run in order.
+
+        // To run multiple commands at the same time,
+        // use addParallel()
+        // e.g. addParallel(new Command1());
+        //      addSequential(new Command2());
+        // Command1 and Command2 will run in parallel.
+
+        // A command group will require all of the subsystems that each member
+        // would require.
+        // e.g. if Command1 requires chassis, and Command2 requires arm,
+        // a CommandGroup containing them would require both the chassis and the
+        // arm.
+    }
+}
--- SOTABots2016/src/org/usfirst/frc/team2557/robot/subsystems/Autonomous.java
+++ SOTABots2016/src/org/usfirst/frc/team2557/robot/subsystems/Autonomous.java
@@ -147,9 +147,33 @@ public class Autonomous extends Subsystem {
     	}
     }
     
+    public void roughTerrainRotation(){
+    	if (RobotMap.RangeFinderF.getVoltage()>4 && RobotMap.RangeFinderL.getVoltage()<=12 && RobotMap.RangeFinderR.getVoltage()<=16){
+    		RobotMap.arcadeDrive.arcadeDrive(1, 0);
+    	}
+    	else if (RobotMap.RangeFinderF.getVoltage()>4 && RobotMap.RangeFinderL.getVoltage()>12 && RobotMap.RangeFinderR.getVoltage()<16){
+    		RobotMap.arcadeDrive.arcadeDrive(1, 1);
+    	}
+    	else if (RobotMap.RangeFinderF.getVoltage()<=4 && RobotMap.RangeFinderL.getVoltage()>12 && RobotMap.RangeFinderR.getVoltage()<16){
+    		RobotMap.arcadeDrive.arcadeDrive(.5, 1);
+    	}
+    }
+    
     public void sallyPort(){
     	if(RobotMap.RangeFinderF.getVoltage()>4){
     		RobotMap.arcadeDrive.arcadeDrive(1, 0);
+    	}
+    }
+    
+    public void sallyPortRotation(){
+    	if (RobotMap.RangeFinderF.getVoltage()>4 && RobotMap.RangeFinderL.getVoltage()<=12 && RobotMap.RangeFinderR.getVoltage()<=16){
+    		RobotMap.arcadeDrive.arcadeDrive(1, 0);
+    	}
+    	else if (RobotMap.RangeFinderF.getVoltage()>4 && RobotMap.RangeFinderL.getVoltage()>12 && RobotMap.RangeFinderR.getVoltage()<16){
+    		RobotMap.arcadeDrive.arcadeDrive(1, 1);
+    	}
+    	else if (RobotMap.RangeFinderF.getVoltage()<=4 && RobotMap.RangeFinderL.getVoltage()>12 && RobotMap.RangeFinderR.getVoltage()<16){
+    		RobotMap.arcadeDrive.arcadeDrive(.5, 1);
     	}
     }
     
