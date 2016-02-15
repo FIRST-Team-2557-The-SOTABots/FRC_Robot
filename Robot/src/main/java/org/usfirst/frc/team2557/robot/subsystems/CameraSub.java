bdec05e8ff974c81ab0cfe1156414ed0c386922e
Philip Rader <pjrader1@yahoo.com>
2016-02-14 19:27:05 -0800

    Added offset and distance calculations to Camera subsystem
    http://wpilib.screenstepslive.com/s/4485/m/24194/l/288985-identifying-and-processing-the-targets

--- Robot/src/main/java/org/usfirst/frc/team2557/robot/subsystems/CameraSub.java
+++ Robot/src/main/java/org/usfirst/frc/team2557/robot/subsystems/CameraSub.java
@@ -2,8 +2,12 @@ package org.usfirst.frc.team2557.robot.subsystems;
 
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+import edu.wpi.first.wpilibj.tables.ITable;
 
 public class CameraSub extends Subsystem {
+
+    private static ITable gripTable = NetworkTable.getTable("GRIP");
 
     public CameraSub() {
         super();
@@ -14,5 +18,18 @@ public class CameraSub extends Subsystem {
 
     }
 
+    public double getTargetPositionX() {
+        final double Px = gripTable.getSubTable("goalContoursReport").getNumber("centerX", 0);
+
+        return (Px - (FOVp / 2)) / (FOVp / 2);
+    }
+
+    private final double fov = 68.5;
+    private final double FOVp = 320;
+    private final double Tft = 1.6667;
+    public double getTargetDistance() {
+        final double Tp = gripTable.getSubTable("goalContoursReport").getNumber("width", 0);
+        return (Tft * FOVp) / (2 * Tp * Math.tan(Math.toRadians(fov / 2)));
+    }
 
 }
