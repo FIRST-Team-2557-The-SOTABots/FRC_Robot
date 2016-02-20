4039a58970b52bf3be6abb4b770f2f4aef6ed3f4
VaneRaklan <antoniowii@yahoo.com>
2016-02-19 17:58:47 -0800

    I edited the arm configuration command so that it matches the POV values as well as has correct parameters for each of the arm configs.

--- Robot/src/main/java/org/usfirst/frc/team2557/robot/commands/ArmConfigurationCommand.java
+++ Robot/src/main/java/org/usfirst/frc/team2557/robot/commands/ArmConfigurationCommand.java
@@ -14,65 +14,45 @@ public class ArmConfigurationCommand extends Command {
 
 	@Override
 	protected void execute() {
-		if(Robot.oi.manipulatorBack.get()){
-			Robot.manipulatorSub.setActuators(-0.35);
-		}
-		else if(Robot.oi.manipulatorStart.get()){
-			Robot.manipulatorSub.setActuators(0.35);
-		}
-		else{
-			Robot.manipulatorSub.setActuators(0);
-		}
-		
-		
-		
-//		if(Robot.oi.manipulator.getPOV() == 0){
-//			if(RobotMap.leftActuatorMotor.getEncPosition() < 0){
-//				Robot.manipulatorSub.actuatorsUp();
-//			}
-//			else if(RobotMap.leftActuatorMotor.getEncPosition() > 0){
-//				Robot.manipulatorSub.actuatorsDown();
-//			}
-//			else{
-//				return;
-//			}
+//		if(Robot.oi.manipulatorBack.get()){
+//			Robot.manipulatorSub.setActuators(-0.35);
 //		}
-//		else if(Robot.oi.manipulator.getPOV() == 2){
-//			if(RobotMap.leftActuatorMotor.getEncPosition() < 45){
-//				Robot.manipulatorSub.actuatorsUp();
-//			}
-//			else if(RobotMap.leftActuatorMotor.getEncPosition() > 45){
-//				Robot.manipulatorSub.actuatorsDown();
-//			}
-//			else{
-//				return;
-//			}
-//		}
-//		else if(Robot.oi.manipulator.getPOV() == 4){
-//			if(RobotMap.leftActuatorMotor.getEncPosition() < 45){
-//				Robot.manipulatorSub.actuatorsUp();
-//			}
-//			else if(RobotMap.leftActuatorMotor.getEncPosition() > 45){
-//				Robot.manipulatorSub.actuatorsDown();
-//			}
-//			else{
-//				return;
-//			}
-//		}
-//		else if(Robot.oi.manipulator.getPOV() == 6){
-//			if(RobotMap.leftActuatorMotor.getEncPosition() < 45){
-//				Robot.manipulatorSub.actuatorsUp();
-//			}
-//			else if(RobotMap.leftActuatorMotor.getEncPosition() > 45){
-//				Robot.manipulatorSub.actuatorsDown();
-//			}
-//			else{
-//				return;
-//			}
+//		else if(Robot.oi.manipulatorStart.get()){
+//			Robot.manipulatorSub.setActuators(0.35);
 //		}
 //		else{
 //			Robot.manipulatorSub.setActuators(0);
 //		}
+		
+		
+		
+		if(Robot.oi.manipulator.getPOV() == 0){
+				if(RobotMap.leftActuatorMotor.getEncPosition()/900 < 0){
+					Robot.manipulatorSub.setActuators(-.5);
+				}	else if(RobotMap.leftActuatorMotor.getEncPosition()/900 > 0){
+					Robot.manipulatorSub.setActuators(.5);
+				}
+		}	else if(Robot.oi.manipulator.getPOV() == 90){
+				if(RobotMap.leftActuatorMotor.getEncPosition()/900 < -35){
+					Robot.manipulatorSub.setActuators(-.5);
+				}	else if(RobotMap.leftActuatorMotor.getEncPosition()/900 > -35){
+					Robot.manipulatorSub.setActuators(.5);
+				}
+		}	else if(Robot.oi.manipulator.getPOV() == 180){
+				if(RobotMap.leftActuatorMotor.getEncPosition()/900 < -150){
+					Robot.manipulatorSub.setActuators(-.5);
+				}	else if(RobotMap.leftActuatorMotor.getEncPosition()/900 > -150){
+					Robot.manipulatorSub.setActuators(.5);
+			}
+		}	else if(Robot.oi.manipulator.getPOV() == 270){
+				if(RobotMap.leftActuatorMotor.getEncPosition()/900 < 45){
+					Robot.manipulatorSub.setActuators(-.5);
+				}	else if(RobotMap.leftActuatorMotor.getEncPosition()/900 > 45){
+					Robot.manipulatorSub.setActuators(.5);
+				}
+		}	else{
+				Robot.manipulatorSub.setActuators(0);
+		}
 
 	}
 
