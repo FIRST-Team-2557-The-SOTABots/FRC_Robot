56f139125e8b324354ebe67cd631538af768c6d0
Philip Rader <pjrader1@yahoo.com>
2016-02-26 20:19:34 -0800

    Added driveStraight method to Chassis subsystem

--- Robot/src/main/java/org/usfirst/frc/team2557/robot/subsystems/Chassis.java
+++ Robot/src/main/java/org/usfirst/frc/team2557/robot/subsystems/Chassis.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team2557.robot.subsystems;
 
+import edu.wpi.first.wpilibj.interfaces.Gyro;
 import org.usfirst.frc.team2557.robot.Robot;
 import org.usfirst.frc.team2557.robot.RobotMap;
 
@@ -10,6 +11,7 @@ import org.usfirst.frc.team2557.robot.commands.chassis.DriveCommand;
 public class Chassis extends Subsystem {
 
     RobotDrive drive = RobotMap.robotDrive;
+    Gyro gyro = RobotMap.mainGyro;
 
     double limitingFactor = 0.7;
 
@@ -26,12 +28,24 @@ public class Chassis extends Subsystem {
                 -Robot.oi.driver.getRawAxis(5) * limitingFactor);
     }
 
+    private final double Kp = 0.03;
+    public void driveStraight(double speed) {
+        this.set(speed, speed * this.getGyroAngle() * Kp);
+    }
+
     public void set(double lvalue, double rvalue) {
         drive.tankDrive(lvalue, rvalue);
     }
 
     public void stop() {
         this.set(0, 0);
+    }
+
+    public void resetGyro() {
+        gyro.reset();
+    }
+    public double getGyroAngle() {
+        return gyro.getAngle();
     }
 }
 
