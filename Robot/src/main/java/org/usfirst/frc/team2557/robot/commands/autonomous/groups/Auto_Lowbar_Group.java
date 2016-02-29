a0561bbe4e5d87e5241ef9347c21f6aab6353459
Philip Rader <pjrader1@yahoo.com>
2016-02-29 15:17:56 -0800

    Commented out Auto sequences in Lowbar autonomous
    These are commented for reference, they will be replaced later.

--- Robot/src/main/java/org/usfirst/frc/team2557/robot/commands/autonomous/groups/Auto_Lowbar_Group.java
+++ Robot/src/main/java/org/usfirst/frc/team2557/robot/commands/autonomous/groups/Auto_Lowbar_Group.java
@@ -14,14 +14,14 @@ public class Auto_Lowbar_Group extends CommandGroup {
 
         // TODO: Edit this sequence, it probably requires more steps than this
 
-        this.addParallel(new Auto_ArmPosition(-35));
+//        this.addParallel(new Auto_ArmPosition(-35));
         this.addParallel(new CatapultRetractCommand());
         this.addSequential(new Auto_DriveToDefense());
 
         this.addSequential(new DistanceDriveCommand(1, 0.5));
 
-        this.addParallel(new Auto_ArmPosition(0));
-        this.addSequential(new Auto_CameraAlignForShot());
+//        this.addParallel(new Auto_ArmPosition(0));
+//        this.addSequential(new Auto_CameraAlignForShot());
 
         this.addSequential(new CatapultShootCommand());
     }
