22c84afc24ca725628fea90c0326f12d5a35288e
Philip Rader <pjrader1@yahoo.com>
2016-02-29 15:18:14 -0800

    Sped up the intake

--- Robot/src/main/java/org/usfirst/frc/team2557/robot/commands/intake/IntakeInCommand.java
+++ Robot/src/main/java/org/usfirst/frc/team2557/robot/commands/intake/IntakeInCommand.java
@@ -16,7 +16,7 @@ public class IntakeInCommand extends Command {
 
     @Override
     protected void execute() {
-        Robot.intake.set(0.5);
+        Robot.intake.set(1);
     }
 
     @Override
