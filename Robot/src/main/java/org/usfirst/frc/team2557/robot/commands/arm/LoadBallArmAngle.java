700dad077607581042d6aaf830cbf564b04ab8a5
Philip Rader <pjrader1@yahoo.com>
2016-02-29 22:14:37 -0800

    Added angle for the arm for loading a ball
    NEEDS THE CORRECT VOLTAGE!

---
+++ Robot/src/main/java/org/usfirst/frc/team2557/robot/commands/arm/LoadBallArmAngle.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team2557.robot.commands.arm;
+
+public class LoadBallArmAngle extends MoveArmToAngleCommand {
+
+    public LoadBallArmAngle() {
+        // Change this "0" to the left actuator voltage
+        // that the arm is located at
+        super(0);
+    }
+
+}
