6ef50917ee3d2503405a30562c1012364bcf52ed
Philip Rader <pjrader1@yahoo.com>
2016-03-04 00:38:12 -0800

    Added potentiometer voltage for loading ball

--- Robot/src/main/java/org/usfirst/frc/team2557/robot/commands/arm/LoadBallArmAngle.java
+++ Robot/src/main/java/org/usfirst/frc/team2557/robot/commands/arm/LoadBallArmAngle.java
@@ -3,9 +3,8 @@ package org.usfirst.frc.team2557.robot.commands.arm;
 public class LoadBallArmAngle extends MoveArmToAngleCommand {
 
     public LoadBallArmAngle() {
-        // Change this "0" to the left actuator voltage
-        // that the arm is located at
-        super(0);
+        // Left actuator potentiometer value
+        super(2.648);
     }
 
 }
