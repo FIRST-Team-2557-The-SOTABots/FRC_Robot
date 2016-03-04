40781d37bf7434731bd4547b4a2d476ed556103d
Philip Rader <pjrader1@yahoo.com>
2016-03-04 14:41:22 -0800

    Inverted joystick speed modifier

--- Robot/src/main/java/org/usfirst/frc/team2557/robot/commands/chassis/DriveCommand.java
+++ Robot/src/main/java/org/usfirst/frc/team2557/robot/commands/chassis/DriveCommand.java
@@ -18,7 +18,7 @@ public class DriveCommand extends Command {
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
         boolean fast = false;
-        double speedEdit = (-Math.abs(Robot.oi.driver.getRawAxis(3) + 1) / 2);
+        double speedEdit = 1 - (-Math.abs(Robot.oi.driver.getRawAxis(3) + 1) / 2);
 //        if(Robot.oi.manipulatorA.get()
 //                && Robot.oi.manipulatorB.get()
 //                && Robot.oi.manipulatorX.get()
