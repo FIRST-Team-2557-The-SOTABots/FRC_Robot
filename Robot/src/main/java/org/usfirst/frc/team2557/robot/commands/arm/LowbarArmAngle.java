58180819578261820f9cc044c23363af7ab44502
Philip Rader <pjrader1@yahoo.com>
2016-03-04 00:38:22 -0800

    Added command and value for lowbar (arm)

---
+++ Robot/src/main/java/org/usfirst/frc/team2557/robot/commands/arm/LowbarArmAngle.java
@@ -0,0 +1,9 @@
+package org.usfirst.frc.team2557.robot.commands.arm;
+
+public class LowbarArmAngle extends MoveArmToAngleCommand {
+
+    public LowbarArmAngle() {
+        super(4.081);
+    }
+
+}
