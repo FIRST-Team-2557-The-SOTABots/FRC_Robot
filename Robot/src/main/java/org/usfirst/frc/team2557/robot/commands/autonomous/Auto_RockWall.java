1cc464172e64706ba444bbda73eb6f8764aca3a4
Philip Rader <pjrader1@yahoo.com>
2016-03-08 17:45:36 -0800

    Added autonomuos for the rock wall

---
+++ Robot/src/main/java/org/usfirst/frc/team2557/robot/commands/autonomous/Auto_RockWall.java
@@ -0,0 +1,22 @@
+package org.usfirst.frc.team2557.robot.commands.autonomous;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import org.usfirst.frc.team2557.robot.commands.arm.MoveArmToAngleCommand;
+import org.usfirst.frc.team2557.robot.commands.automation.Auto_LoadBall;
+import org.usfirst.frc.team2557.robot.commands.autonomous.sequences.Auto_CameraShootSequence;
+import org.usfirst.frc.team2557.robot.commands.chassis.DistanceDriveCommand;
+import org.usfirst.frc.team2557.robot.subsystems.Arm;
+
+public class Auto_RockWall extends CommandGroup {
+
+    public Auto_RockWall() {
+
+        this.addSequential(new MoveArmToAngleCommand(Arm.ARM_ROUGH_DEFENSE)); // Move the arm to a relatively high angle
+        this.addSequential(new Auto_DriveToDefense()); // Drive the the defense
+        this.addSequential(new DistanceDriveCommand(1.5, 1)); // Drive over the rock wall
+        this.addSequential(new Auto_LoadBall()); // Load ball!
+        this.addSequential(new Auto_CameraShootSequence());
+
+    }
+
+}
