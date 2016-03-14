7dd88b834b2f38ce7e278e137891b28b9791a220
Philip Rader <pjrader1@yahoo.com>
2016-03-13 22:50:21 -0700

    Changed camera correction commands to use PIDs
    Added correct min/max shot distance, in meters

--- Robot/src/main/java/org/usfirst/frc/team2557/robot/commands/camera/CorrectDistanceToTargetCommand.java
+++ Robot/src/main/java/org/usfirst/frc/team2557/robot/commands/camera/CorrectDistanceToTargetCommand.java
@@ -1,37 +1,66 @@
 package org.usfirst.frc.team2557.robot.commands.camera;
 
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.PIDSourceType;
 import edu.wpi.first.wpilibj.command.Command;
 import org.usfirst.frc.team2557.robot.Robot;
 import org.usfirst.frc.team2557.robot.subsystems.Camera;
 
 public class CorrectDistanceToTargetCommand extends Command {
 
-    private final double minShotDistance = 3;
-    private final double maxShotDistnace = 4;
+    private final double minShotDistance = 1.98; // Meters!
+    private final double maxShotDistance = 3.5; // Meters!
 
-    private double speed = 1;
+    private PIDController _controlller;
 
     public CorrectDistanceToTargetCommand() {
         requires(Robot.chassis);
         requires(Robot.camera);
+
+        // Kp, Ki, Kd
+        this._controlller = new PIDController(0.2, 0.1, 0, new PIDSource() {
+            @Override
+            public void setPIDSourceType(PIDSourceType pidSource) {
+            }
+
+            @Override
+            public PIDSourceType getPIDSourceType() {
+                return PIDSourceType.kDisplacement;
+            }
+
+            @Override
+            public double pidGet() {
+                Camera.Target target = getTarget();
+                if(target != null) {
+                    return target.distance;
+                }
+
+                return (minShotDistance + maxShotDistance) / 2;
+            }
+        }, new PIDOutput() {
+            @Override
+            public void pidWrite(double output) {
+                Robot.chassis.driveStraight(output);
+            }
+        });
+
+        this._controlller.setInputRange(0, 5);
+        this._controlller.setOutputRange(-1, 1);
+        this._controlller.setPercentTolerance(10);
     }
 
     @Override
     protected void initialize() {
-
+        this._controlller.reset();
+        this._controlller.setSetpoint((minShotDistance + maxShotDistance) / 2);
+        Robot.chassis.initDriveStraight();
     }
 
     @Override
     protected void execute() {
-        Camera.Target target = this.getTarget();
-        if(target != null) {
-            Robot.chassis.driveStraight(this.manipulateWithDistance(target.distance));
-        }
-    }
-
-    private double manipulateWithDistance(double distance) {
-        double x = distance - (minShotDistance + maxShotDistnace) / 2 + 3;
-        return (x * x - 10) / (x * x);
+        this._controlller.enable();
     }
 
     private Camera.Target getTarget() {
@@ -42,7 +71,7 @@ public class CorrectDistanceToTargetCommand extends Command {
             if(trackedTarget != null) {
 //                if(trackedTarget.area < target.area
 //                        || trackedTarget.solidity < target.solidity) {
-                if(trackedTarget.width < target.width) {
+                if(target.width > trackedTarget.width) {
                     trackedTarget = target;
                 }
             }else{
@@ -56,13 +85,14 @@ public class CorrectDistanceToTargetCommand extends Command {
     @Override
     protected boolean isFinished() {
         if(this.getTarget() == null)
-            return true;
+            return true; // This should never happen if TurnToTarget is called first
 
-        return this.getTarget().distance >= minShotDistance && this.getTarget().distance <= maxShotDistnace;
+        return this._controlller.onTarget();
     }
 
     @Override
     protected void end() {
+        this._controlller.disable();
         Robot.chassis.set(0, 0);
     }
 
