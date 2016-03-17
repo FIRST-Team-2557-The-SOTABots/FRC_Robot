5f146a0911dc44c92473594b2b80bbdc7af7486a
Philip Rader <pjrader1@yahoo.com>
2016-03-16 20:34:07 -0700

    Modified camera shoot sequence

--- Robot/src/main/java/org/usfirst/frc/team2557/robot/commands/autonomous/sequences/Auto_CameraShootSequence.java
+++ Robot/src/main/java/org/usfirst/frc/team2557/robot/commands/autonomous/sequences/Auto_CameraShootSequence.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team2557.robot.commands.autonomous.sequences;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
 import org.usfirst.frc.team2557.robot.commands.arm.MoveArmToAngleCommand;
 import org.usfirst.frc.team2557.robot.commands.camera.CorrectDistanceToTargetCommand;
 import org.usfirst.frc.team2557.robot.commands.camera.TurnToTargetCommand;
@@ -10,13 +11,13 @@ import org.usfirst.frc.team2557.robot.subsystems.Arm;
 public class Auto_CameraShootSequence extends CommandGroup {
 
     public Auto_CameraShootSequence() {
-
+        this.addSequential(new MoveArmToAngleCommand(Arm.ARM_LOADBALL));
         this.addSequential(new TurnToTargetCommand()); // Turn to the target
         this.addSequential(new CorrectDistanceToTargetCommand()); // Correct our distance to the target
+        this.addSequential(new WaitCommand(1.5)); // Wait for camera to update
         this.addSequential(new TurnToTargetCommand()); // Double check that we are aligned
-        this.addSequential(new MoveArmToAngleCommand(Arm.ARM_LOWBAR)); // Lower the arm to the floor
+        this.addSequential(new MoveArmToAngleCommand(Arm.ARM_BOTTOM)); // Lower the arm to the floor
         this.addSequential(new CatapultShootCommand()); // Shoot the ball!
-
     }
 
 }
