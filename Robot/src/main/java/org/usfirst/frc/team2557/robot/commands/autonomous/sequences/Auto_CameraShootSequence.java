3a1da8a0e224bc354bf1f08bcb1cd673226cb52d
Philip Rader <pjrader1@yahoo.com>
2016-03-23 23:09:39 -0700

    Stream-lined the camera shoot command to speed up the command

--- Robot/src/main/java/org/usfirst/frc/team2557/robot/commands/autonomous/sequences/Auto_CameraShootSequence.java
+++ Robot/src/main/java/org/usfirst/frc/team2557/robot/commands/autonomous/sequences/Auto_CameraShootSequence.java
@@ -11,12 +11,8 @@ import org.usfirst.frc.team2557.robot.subsystems.Arm;
 public class Auto_CameraShootSequence extends CommandGroup {
 
     public Auto_CameraShootSequence() {
-//        this.addSequential(new MoveArmToAngleCommand(Arm.ARM_LOADBALL));
         this.addSequential(new TurnToTargetCommand()); // Turn to the target
-        this.addSequential(new CorrectDistanceToTargetCommand()); // Correct our distance to the target
-        this.addSequential(new WaitCommand(1.5)); // Wait for camera to update
-        this.addSequential(new TurnToTargetCommand()); // Double check that we are aligned
-        this.addSequential(new MoveArmToAngleCommand(Arm.ARM_BOTTOM)); // Lower the arm to the floor
+//        this.addSequential(new MoveArmToAngleCommand(Arm.ARM_BOTTOM)); // Lower the arm to the floor
         this.addSequential(new CatapultShootCommand()); // Shoot the ball!
     }
 
