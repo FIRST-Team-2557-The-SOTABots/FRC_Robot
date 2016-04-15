0060817334bfb8e5aff0fde092a51859c28889a4
Philip Rader <pjrader1@yahoo.com>
2016-04-14 20:41:34 -0700

    Added more encoder drive methods in Chassis subsystem

--- Robot/src/main/java/org/usfirst/frc/team2557/robot/subsystems/Chassis.java
+++ Robot/src/main/java/org/usfirst/frc/team2557/robot/subsystems/Chassis.java
@@ -10,13 +10,19 @@ import org.usfirst.frc.team2557.robot.commands.chassis.DriveCommand;
 
 public class Chassis extends Subsystem {
 
-    CANTalon leftEncTalon = RobotMap.driveLeft2;
-    CANTalon rightEncTalon = RobotMap.driveRight1;
+//    CANTalon leftEncTalon = RobotMap.driveLeft2;
+//    CANTalon rightEncTalon = RobotMap.driveRight1;
+    // TODO: See whether the comp bot's encoders are switched as well
+    CANTalon leftEncTalon = RobotMap.driveRight1; // Right and left are switched on the practice bot
+    CANTalon rightEncTalon = RobotMap.driveLeft2;
+
     RobotDrive drive = RobotMap.robotDrive;
     Gyro gyro = RobotMap.mainGyro;
 
     double leftPosResetValue = 0;
     double rightPosResetValue = 0;
+
+    private final double posToMeters = (14 / 28) * 8 * 3.14159;
     
     public Chassis() {
     	
@@ -42,14 +48,13 @@ public class Chassis extends Subsystem {
         this.set(0, 0);
     }
 
-    // NOTE: Works best when the robot has just driven straight
+    // NOTE: Only works when the robot has just driven straight
     public double getDistanceTraveled() {
         // Return the average of the distance between both encoders
         double posToMeters = (14 / 28) * 8 * 3.14159;
-        return ((double) leftEncTalon.getEncPosition() - leftPosResetValue
-                + (double) rightEncTalon.getEncPosition() - rightPosResetValue)
-                * 0.5
-                * posToMeters;
+        return (this.getLeftEncoderPos()
+                + this.getRightEncoderPos())
+                * 0.5;
     }
     public void resetDistanceTraveled() {
         leftPosResetValue = (double) leftEncTalon.getEncPosition();
@@ -59,9 +64,22 @@ public class Chassis extends Subsystem {
     public double getLeftEncoderVel() {
         return leftEncTalon.getEncVelocity();
     }
-
     public double getRightEncoderVel() {
-        return rightEncTalon.getEncVelocity();
+        return -rightEncTalon.getEncVelocity(); // Right encoder is negative (spins reverse when the bot is moving forwards)
+    }
+
+    public double getLeftEncoderPos() {
+        return leftEncTalon.getEncPosition() - leftPosResetValue;
+    }
+    public double getRightEncoderPos() {
+        return -(rightEncTalon.getEncPosition() - rightPosResetValue); // Right encoder is negative (spins reverse when the bot is moving forwards)
+    }
+
+    public double getLeftEncoderDistance() {
+        return this.getLeftEncoderPos() * posToMeters;
+    }
+    public double getRightEncoderDistance() {
+        return this.getRightEncoderPos() * posToMeters;
     }
 
     public void resetGyro() {
