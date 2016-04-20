461ee0836013623975e218988c927a2c3faeaff4
Philip Rader <pjrader1@yahoo.com>
2016-04-19 21:29:22 -0700

    Added sequences for positions and extra auto for lowbar
    Position 1 is the right of the lowbar from the driver station

---
+++ Robot/src/main/java/org/usfirst/frc/team2557/robot/commands/autonomous/Auto_LowbarCenter.java
@@ -0,0 +1,24 @@
+package org.usfirst.frc.team2557.robot.commands.autonomous;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import org.usfirst.frc.team2557.robot.commands.arm.MoveArmToAngleCommand;
+import org.usfirst.frc.team2557.robot.commands.catapult.CatapultRetractCommand;
+import org.usfirst.frc.team2557.robot.commands.chassis.EncoderPosDriveCommand;
+import org.usfirst.frc.team2557.robot.subsystems.Arm;
+
+public class Auto_LowbarCenter extends CommandGroup {
+
+    public Auto_LowbarCenter() {
+
+        // Parallels that run with a sequential
+        // are added before that sequential!
+
+        this.addParallel(new CatapultRetractCommand());
+        this.addSequential(new MoveArmToAngleCommand(Arm.ARM_LOWBAR));
+
+        this.addSequential(new EncoderPosDriveCommand(18282, 0.5));
+
+        // TODO: Center batter shot sequence
+    }
+
+}
---
+++ Robot/src/main/java/org/usfirst/frc/team2557/robot/commands/autonomous/sequences/Auto_Pos3Center.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team2557.robot.commands.autonomous.sequences;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class Auto_Pos3Center extends CommandGroup {
+
+    public Auto_Pos3Center() {
+
+    }
+
+}
